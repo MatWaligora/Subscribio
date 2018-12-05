87d471d188260f4d7fe5e8bf9eb8f90c659889cb
Mateusz Waligóra <matt.waligora@gmail.com>
2018-12-05 17:09:41 +0100

    Added CRUD for subscriptions in reducer and corresponding actions

--- src/store/reducers/subscriptions.js
+++ src/store/reducers/subscriptions.js
@@ -1,3 +1,5 @@
+import * as actionTypes from '../actions/actionTypes';
+
 const initialState = {
   subscriptions: [
       {
@@ -12,6 +14,41 @@ const initialState = {
   ]
 };
 
+const addSubscription = (state, action) => {
+  const subscriptions = state.subscriptions.slice();
+  subscriptions.push(action.subscription);
+  return {
+    ...state,
+    subscriptions
+  }
+};
+
+const removeSubscription = (state, action) => {
+  const subscriptions = state.subscriptions.slice();
+  const { subscription } = action;
+  subscriptions.splice(1, subscriptions.indexOf(sub => sub.id === subscription.id));
+  return {
+    ...state,
+    subscriptions
+  }
+};
+
+const updateSubscription = (state, action) => {
+  const subscriptions = state.subscriptions.slice();
+  const { subscription } = action;
+  subscriptions[subscriptions.indexOf(sub => sub.id === subscription.id)] = subscription;
+  return {
+    ...state,
+    subscriptions
+  }
+};
+
 export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case [actionTypes.ADD_SUBSCRIPTION]: return addSubscription(state, action);
+    case [actionTypes.REMOVE_SUBSCRIPTION]: return removeSubscription(state, action);
+    case [actionTypes.UPDATE_SUBSCRIPTION]: return updateSubscription(state, action);
+
+  }
   return state;
 };
