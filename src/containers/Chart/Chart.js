c0906f155d9eecffd842dda1a6874d12db9055c4
Mateusz Waligóra <matt.waligora@gmail.com>
2018-12-10 12:40:58 +0100

    Added responsiveness for charts

--- src/containers/Chart/Chart.js
+++ src/containers/Chart/Chart.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Line, LineChart, XAxis, YAxis, Tooltip } from 'recharts';
+import { Line, LineChart, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import { connect } from 'react-redux';
 import moment from 'moment';
 import './Chart.css';
@@ -40,12 +40,14 @@ class chart extends Component {
     });
     return (
       <div className="Chart">
-        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 15, left: 5 }}>
-          <Line type="monotone" dataKey="value" stroke="#f68113" />
-          <XAxis dataKey="name" label={{ value: "Month", position: 'insideBottom', offset: -10, stroke: '#f68113' }}/>
-          <YAxis dataKey="value" label="Value" label={{ value: "Value", angle: -90, position: 'insideLeft',stroke: '#f68113' }}/>
-          <Tooltip />
-        </LineChart>
+        <ResponsiveContainer width="100%" height={300}>
+          <LineChart data={data} margin={{ top: 5, right: 20, bottom: 15, left: 5 }}>
+            <Line type="monotone" dataKey="value" stroke="#f68113" />
+            <XAxis dataKey="name" label={{ value: "Month", position: 'insideBottom', offset: -10, stroke: '#f68113' }}/>
+            <YAxis dataKey="value" label="Value" label={{ value: "Value", angle: -90, position: 'insideLeft',stroke: '#f68113' }}/>
+            <Tooltip />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     )
   }
